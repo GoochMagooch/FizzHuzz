9c6fec68bcd7a048a48c8b6123ef4415deae52a3
Magooch <crvallecito@gmail.com>
2026-02-12 23:22:17 -0700

    First iteration of FizzHuzz. Conditions aren't quite following classic FizzBuzz constraints

---
+++ FizzHuzz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int num = 0;
+
+    printf("Enter a number from 1 to 100: ");
+    scanf("%d", &num);
+
+    if (num >= 1 && num <= 100) {
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                printf("FizzHuzz!\n");
+            } else if (i % 3 == 0) {
+                printf("Fizz\n");
+            } else if (i % 5 == 0) {
+                printf("Huzz\n");
+            }
+        }
+    } else {
+        printf("Enter a number between 1 and 100! >:[\n");
+    }
+
+    return 0;
+}
